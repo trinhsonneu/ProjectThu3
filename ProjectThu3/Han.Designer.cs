949f5725a26198d676999ea6d08fbb77ca199f4d
admin <admin@DESKTOP-16RTLLC>
2025-09-09 16:40:07 +0700

    cong viec cua Han dam nhiem

--- ProjectThu3/Han.Designer.cs
+++ ProjectThu3/Han.Designer.cs
@@ -34,6 +34,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
             this.label1.Location = new System.Drawing.Point(223, 71);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(142, 20);
