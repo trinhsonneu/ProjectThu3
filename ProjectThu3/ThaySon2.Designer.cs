424a5e3bd54e5394b1d6254968c35a56444a1abe
trinhsonneu <trinhson@gmail.com>
2025-09-15 13:25:05 +0700

    nbn

---
+++ ProjectThu3/ThaySon2.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace ProjectThu3
+{
+    partial class ThaySon2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblkq = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(15, 380);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(301, 48);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "Giai phuong trinh";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(623, 404);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(165, 34);
+            this.btnEnd.TabIndex = 1;
+            this.btnEnd.Text = "Ket thuc";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(21, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(295, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Giai Phuong trinh bac 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(244, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nhap so lieu dau vao:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 26);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "a=";
+            // 
+            // txta
+            // 
+            this.txta.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txta.Location = new System.Drawing.Point(87, 102);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(55, 32);
+            this.txta.TabIndex = 5;
+            this.txta.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtb
+            // 
+            this.txtb.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtb.Location = new System.Drawing.Point(241, 103);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(55, 32);
+            this.txtb.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(190, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 26);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "b=";
+            // 
+            // txtc
+            // 
+            this.txtc.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtc.Location = new System.Drawing.Point(404, 104);
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(55, 32);
+            this.txtc.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(353, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 26);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "c=";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(55, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 31);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Ket qua:";
+            // 
+            // lblkq
+            // 
+            this.lblkq.AutoSize = true;
+            this.lblkq.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblkq.ForeColor = System.Drawing.Color.Red;
+            this.lblkq.Location = new System.Drawing.Point(103, 225);
+            this.lblkq.Name = "lblkq";
+            this.lblkq.Size = new System.Drawing.Size(79, 29);
+            this.lblkq.TabIndex = 11;
+            this.lblkq.Text = "label7";
+            // 
+            // ThaySon2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblkq);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtc);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txta);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnOK);
+            this.Name = "ThaySon2";
+            this.Text = "ThaySon2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblkq;
+    }
+}
---
+++ ProjectThu3/ThaySon2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectThu3
+{
+    public partial class ThaySon2 : Form
+    {
+        double a, b, c, d, x1, x2;
+        public ThaySon2()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                a = Convert.ToDouble(txta.Text);
+                b = Convert.ToDouble(txtb.Text);
+                c = Convert.ToDouble(txtc.Text);
+                d = b * b - 4 * a * c;
+                if (d < 0)
+                {
+                    //  MessageBox.Show("Phương trình vô nghiệm");
+                    lblkq.Text = "Phương trình vô nghiệm";
+                }
+                else if (d == 0)
+                {
+                    x1 = -b / (2 * a);
+                    //  MessageBox.Show("Phương trình có nghiệm kép: x1=x2=" + x1);
+                    lblkq.Text = "Phương trình có nghiệm kép: x1=x2=" + Math.Round(x1, 1);
+                }
+                else
+                {
+                    x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                    //  MessageBox.Show("Phương trình có 2 nghiệm phân biệt: x1=" + x1 + " và x2=" + x2);
+                    lblkq.Text = "Phương trình có 2 nghiệm phân biệt: x1=" + Math.Round(x1, 1) + " và x2=" + Math.Round(x2, 1);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Dữ liệu nhập vào không hợp lệ");
+            }
+        }
+    }
+}
+    
+
